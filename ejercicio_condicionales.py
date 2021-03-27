e21025ac7e8d9e4d98bd9934a3245dc96ae69be0
fgomez163 <fgomez163@unab.edu.co>
2021-03-26 20:42:01 -0500

    Modificación del for

--- ejercicio_condicionales.py
+++ ejercicio_condicionales.py
@@ -11,8 +11,8 @@ ingredientes_listos = ['chocolate negro', 'harina']
 costo_total = 0
 ahorro = 0
 
-for key, value in lista_ingredientes_brownies.items():
-    if key in ingredientes_listos:
+for ingrediente, value in lista_ingredientes_brownies.items():
+    if ingrediente in ingredientes_listos:
         ahorro = ahorro + value
     else:
         costo_total = costo_total + value
